c9ea5201ceb7437d7c86b97fe1f13cd3717e061b
doankietdev <doankietdev@gmail.com>
2024-07-27 22:15:29 +0700

    feat: config

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
 import { Suspense } from 'react'
 import { Routes } from 'react-router-dom'
+import { ThemeProvider as StyledThemeProvider } from 'styled-components'
+import { toast, ToastContainer } from 'react-toastify'
 
 // styles
 import '~/assets/styles/index.scss'
+import 'react-toastify/dist/ReactToastify.min.css'
+import ThemeStyles from '~/assets/styles/theme.js'
 
 // fonts
 import '~/assets/fonts/icomoon/icomoon.woff'
 
 // components
 import Loader from './Loader'
+import { useTheme } from './contexts/ThemeContext'
 
 function App() {
+  const { theme } = useTheme()
+
+  toast.success('Success')
 
   return (
-    <div className='app'>
-      <div className='app_content'>
-        <div className='main'>
-          <Suspense fallback={<Loader />}>
-            <Routes>
-            </Routes>
-          </Suspense>
+    <StyledThemeProvider theme={{ theme }}>
+      <ThemeStyles />
+      <ToastContainer theme={theme} autoClose={3000} style={{ padding: '20px' }} />
+      <div className='app'>
+        <div className='app_content'>
+          <div className='main'>
+            <Suspense fallback={<Loader />}>
+              <Routes>
+              </Routes>
+            </Suspense>
+          </div>
         </div>
       </div>
-    </div>
+    </StyledThemeProvider>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,13 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import ThemeStyles from '~/assets/styles/theme.js'
-import { ThemeProvider as StyledThemeProvider } from 'styled-components'
 import { ThemeProvider } from '~/contexts/ThemeContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <StyledThemeProvider theme={{ theme: 'light' }}>
-          <ThemeStyles />
-          <App />
-        </StyledThemeProvider>
+        <App />
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
